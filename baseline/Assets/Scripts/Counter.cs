4b780edc2b349134d9908d831168d66bb8ce861c
mattak <mattak.me@gmail.com>
2020-07-15 01:05:41 +0900

    :seedling: create baseline project

---
+++ baseline/Assets/Scripts/Counter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Baseline
+{
+    public class Counter : MonoBehaviour
+    {
+        [SerializeField] private Button Button = default;
+        [SerializeField] private Text Text = default;
+
+        void Start()
+        {
+            var count = PlayerPrefs.GetInt("Count", 0);
+            this.RenderCount(count);
+
+            this.Button.onClick.AddListener(() =>
+            {
+                count += 1;
+                PlayerPrefs.SetInt("Count", count);
+                this.RenderCount(count);
+            });
+        }
+
+        private void RenderCount(int value)
+        {
+            this.Text.text = value.ToString();
+        }
+    }
+}
